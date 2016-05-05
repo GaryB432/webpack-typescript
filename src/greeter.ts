01ca2e37a98584866772b29765bbeb8414ed8d3d
Gary Bortosky <garybort@gmail.com>
2016-05-05 12:53:24 -0500

    use ts helpers

--- src/greeter.ts
+++ src/greeter.ts
@@ -1,5 +1,11 @@
-export class Greeter {
-    constructor(public greeting: string) { }
+abstract class AbstractGreeter{
+    public abstract greet(): string;
+}
+
+export class Greeter extends AbstractGreeter {
+    constructor(public greeting: string) {
+        super();
+     }
     public greet(): string {
         return "<h1>" + this.greeting + "</h1>";
     }
